10ac5a5c464ff267f917343dbcd35b4796431b1d
EvandroChucky <evandrochucky@gmail.com>
2020-08-20 02:27:08 -0300

    Service layer, component registration

--- src/main/java/com/casa/ze/course/resources/UserResource.java
+++ src/main/java/com/casa/ze/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.casa.ze.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.casa.ze.course.entities.User;
+import com.casa.ze.course.services.UserService;
 
 @RestController
 @RequestMapping(value= "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "João", "joao@gmail.com", "9999999", "joao1001");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
