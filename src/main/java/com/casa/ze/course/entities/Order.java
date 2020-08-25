606aa5c238b441e78914a6b5c59e87fb6f70927b
EvandroChucky <evandrochucky@gmail.com>
2020-08-24 23:45:06 -0300

    Subtotal & Total methods

--- src/main/java/com/casa/ze/course/entities/Order.java
+++ src/main/java/com/casa/ze/course/entities/Order.java
@@ -106,6 +106,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		Double sun = 0.0;
+		for(OrderItem x : items) {
+			sun += x.getSubTotal();
+		}
+		return sun;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
