16e14c95f04408cd3383a567aba922cf7c80be63
EvandroChucky <evandrochucky@gmail.com>
2020-08-24 23:35:07 -0300

    Payment, one-to-one association

--- src/main/java/com/casa/ze/course/config/TestConfig.java
+++ src/main/java/com/casa/ze/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.casa.ze.course.entities.Category;
 import com.casa.ze.course.entities.Order;
 import com.casa.ze.course.entities.OrderItem;
+import com.casa.ze.course.entities.Payment;
 import com.casa.ze.course.entities.Product;
 import com.casa.ze.course.entities.User;
 import com.casa.ze.course.entities.enums.OrderStatus;
@@ -84,6 +85,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
--- src/main/java/com/casa/ze/course/entities/Order.java
+++ src/main/java/com/casa/ze/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.casa.ze.course.entities.enums.OrderStatus;
@@ -42,6 +44,10 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	//mapeamento de um para um que deverá ter o mesmo id, por isso utilizamos o CascadeType.ALL
+	@OneToOne (mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -88,6 +94,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
 	public Set<OrderItem> getItems() {
 		return items;
 	}
